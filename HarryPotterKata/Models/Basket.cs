8e03cf816b7ab6d6fa0fb5c94a2693c3b6f5b4be
aosiname <osinamea@gmail.com>
2015-05-29 02:22:50 +0100

    sprint1 - missing file

---
+++ HarryPotterKata/Models/Basket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HarryPotterKata.Models
+{
+    public class Basket : IBookCollection
+    {
+        private List<Book> books { get; set; }
+
+        public Basket()
+        {
+            this.books = new List<Book>();
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return this.books;
+        }
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EmptyBasket()
+        {
+            this.books.Clear();
+        }
+
+        public decimal GetTotalPrice(ICalculator calculator)
+        {
+
+            return calculator.CalculateTotalPrice(this);
+        }
+    }
+}
